b46a41a47c8b6635cda96829dc6d34bc1d3a6948
Andrey Donskoy <donskoi.com@gmail.com>
2022-10-31 14:31:05 +0300

    first commit

---
+++ draw.py
@@ -0,0 +1,26 @@
+import sys
+import matplotlib.pyplot as plt
+
+
+def draw(xs: list, ys: list):
+    plt.figure(figsize=(8, 8))
+    plt.title("Best way", fontsize=20)
+    plt.plot(xs, ys, c='r')
+    plt.scatter(xs, ys, c='black')
+    plt.xticks(xs, [abs(i) for i in xs])
+    plt.yticks(ys, [abs(i) for i in ys])
+    plt.savefig(f'../photo.png')
+
+
+def main():
+    path = "./../result.txt"
+    xs, ys = [], [] 
+    with open(path, 'r') as file:
+        for i in file.readlines():
+            x, y = i.split()
+            xs.append(int(x))
+            ys.append(int(y) * (-1))
+    draw(xs, ys)
+
+if __name__ == "__main__":
+    main()
